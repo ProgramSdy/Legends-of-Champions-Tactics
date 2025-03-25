3ade06c9e39c41356bd69df1bcdb8fe35154d6bd
Daoyu-Shen <126366349+Daoyu-Shen@users.noreply.github.com>
2025-03-25 21:49:05 +1100

    New_Start

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def main():
     #'''
     Player_1 = Death_Knight_Blood(sys_init, "Arthas", "Group_A", True)
     Player_2 = Priest_Shadow(sys_init, "Black Heart", "Group_A", True)
-    Player_3 = Warlock_Destruction(sys_init, "Guldan", "Group_A", True)
+    Player_3 = Paladin_Comprehensiveness(sys_init, "Guldan", "Group_A", True)
     player_heroes = [Player_1,Player_2, Player_3]
     #'''
     
